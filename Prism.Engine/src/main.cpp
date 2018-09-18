13f5014ae164d004efba7ce758bc8db8b8c8080e
daanwillems <dwillems2@avans.nl>
2018-09-18 12:21:14 +0200

    Fixed x64 compilation. NEVER SET THE ENTRY POINT PLS

--- Prism.Engine/src/main.cpp
+++ Prism.Engine/src/main.cpp
@@ -2,7 +2,7 @@
 #include "Window.h"
 
 //QQQ Remove this main method
-int main(int argc, char **argv) {
+int main(int argc, char ** argv) {	
 	SDLFacade::Window window;
 	window.init("Prism", 1280, 720, 100, 100);
 	window.createOpenGLContext(4, 1, true);
