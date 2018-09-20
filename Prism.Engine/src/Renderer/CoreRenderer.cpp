244274bd28e1ab6facf1dbde8aaf5675470bbd51
daanwillems <dwillems2@avans.nl>
2018-09-20 22:12:11 +0200

    Fix a bunch of stuff

--- Prism.Engine/src/Renderer/CoreRenderer.cpp
+++ Prism.Engine/src/Renderer/CoreRenderer.cpp
@@ -18,6 +18,15 @@
 namespace Renderer {
 	glm::mat4 trans = glm::mat4(1.0f);
 
+	glm::mat4 view = glm::lookAt(
+		glm::vec3(3.2f, 0.f, 1.2f),
+		glm::vec3(0.0f, 0.0f, 0.0f),
+		glm::vec3(0.0f, 0.0f, 1.0f)
+	);
+
+
+	glm::mat4 proj = glm::perspective(glm::radians(70.0f), 1920.0f/1080.0f, 1.0f, 10.0f);
+
 	auto t_start = std::chrono::high_resolution_clock::now();
 	CoreRenderer::CoreRenderer()
 	{
@@ -27,26 +36,28 @@ namespace Renderer {
 	void CoreRenderer::init()
 	{
 
-		trans = glm::rotate(trans, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+		trans = glm::rotate(trans, glm::radians(0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 
 		//Get vertex source
 		const char* vertexShaderSource = { \
-			"#version 140\n\
-			in vec2 LVertexPos2D; \
-			uniform mat4 trans;\
+			"#version 330 core\n\
+			layout (location = 0) in vec3 aPos; \
+			uniform mat4 model;\
+			uniform mat4 view;\
+			uniform mat4 proj; \
 			void main() { \
-				gl_Position = trans * vec4( LVertexPos2D.x, LVertexPos2D.y, 0, 1 ); \
+				gl_Position = proj * view * model * vec4(aPos.x, aPos.y, aPos.z, 1 ); \
 			}"
 		};
 	
 
 		const char* fragmentShaderSource =
 		{
-			"#version 140\n\
+			"#version 330 core\n\
 			uniform vec3 triangleColor;\
-			out vec4 LFragment; \
+			out vec4 FragColor; \
 			void main() { \
-				LFragment = vec4(triangleColor, 1.0 ); \
+				FragColor = vec4(triangleColor, 1.0 ); \
 			}"
 		};
 
@@ -58,7 +69,9 @@ namespace Renderer {
 		pipeline = renderDevice->createPipeline(vertexShader, fragmentShader);
 		
 		pipeline->createUniform("triangleColor");
-		pipeline->createUniform("trans");
+		pipeline->createUniform("model");
+		pipeline->createUniform("view");
+		pipeline->createUniform("proj");
 
 		//Do Fragment
 		//End new shaders
@@ -66,40 +79,36 @@ namespace Renderer {
 		//Success flag
 		bool success = true;
 
-		//Get vertex attribute location
-		gVertexPos2DLocation = glGetAttribLocation(pipeline->pipelineID, "LVertexPos2D");
-		if (gVertexPos2DLocation == -1)
-		{
-			printf("LVertexPos2D is not a valid glsl program variable!\n");
-			success = false;
-		}
-		else
-		{
-			//Initialize clear color
-			glClearColor(0.f, 0.f, 0.f, 1.f);
 
-			//VBO data
-			GLfloat vertexData[] =
-			{
-				-0.5f, -0.5f,
-				0.5f, -0.5f,
-				0.5f,  0.5f,
-				-0.5f,  0.5f
-			};
+		//Initialize clear color
+		glClearColor(1.f, 1.f, 1.f, 1.f);
 
-			//IBO data
-			GLuint indexData[] = { 0, 1, 2, 3 };
+		float vertices[] = {
+		 0.5f,  0.5f, -2.0f,  // top right
+		 0.5f, -0.5f, -2.0f,  // bottom right
+		-0.5f, -0.5f, -2.0f,  // bottom left
+		-0.5f,  0.5f, -2.0f   // top left 
+		};
+		unsigned int indices[] = {  // note that we start from 0!
+			0, 1, 3,   // first triangle
+			1, 2, 3    // second triangle
+		};
 
-			//Create VBO
-			glGenBuffers(1, &gVBO);
-			glBindBuffer(GL_ARRAY_BUFFER, gVBO);
-			glBufferData(GL_ARRAY_BUFFER, 2 * 4 * sizeof(GLfloat), vertexData, GL_STATIC_DRAW);
+		//Create VBO
+		glGenBuffers(1, &gVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, gVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-			//Create IBO
-			glGenBuffers(1, &gIBO);
-			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gIBO);
-			glBufferData(GL_ELEMENT_ARRAY_BUFFER, 4 * sizeof(GLuint), indexData, GL_STATIC_DRAW);
-		}
+		//Create IBO
+		glGenBuffers(1, &gIBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gIBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+		//Enable vertex position
+
+		//Set vertex data
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
+		glEnableVertexAttribArray(0);
 
 		return;
 	}
@@ -108,31 +117,28 @@ namespace Renderer {
 	void CoreRenderer::draw()
 	{
 		//Clear color buffer
-		glClear(GL_COLOR_BUFFER_BIT);
-
-
+	glClear(GL_COLOR_BUFFER_BIT);
 
 	//Render quad
 		if (true)
 		{
 			auto t_now = std::chrono::high_resolution_clock::now();
 			float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
+
+
 			pipeline->run();
-			pipeline->setUniformVector("triangleColor", (sin(time * 4.0f) + 1.0f) / 2.0f, 0.0f, 0.0f);
-			pipeline->setUniformMatrix4f("trans", trans);
-			//Enable vertex position
-			glEnableVertexAttribArray(gVertexPos2DLocation);
-
-			//Set vertex data
-			glBindBuffer(GL_ARRAY_BUFFER, gVBO);
-			glVertexAttribPointer(gVertexPos2DLocation, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL);
-
+			pipeline->setUniformVector("triangleColor", 1.0f, 0.0f, 0.0f);
+			pipeline->setUniformMatrix4f("model", trans);
+			pipeline->setUniformMatrix4f("view", view);
+			pipeline->setUniformMatrix4f("proj", proj);
 			//Set index data and render
-			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gIBO);
-			glDrawElements(GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, NULL);
-
+			glm::mat4 trans = glm::mat4(1.0f);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
+			trans = glm::translate(trans, glm::vec3(0.f, 2.f, -1.f));
+			pipeline->setUniformMatrix4f("model", trans);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL);
 			//Disable vertex position
-			glDisableVertexAttribArray(gVertexPos2DLocation);
+			//glDisableVertexAttribArray(gVertexPos2DLocation);
 
 			//Unbind program
 			pipeline->stop();
