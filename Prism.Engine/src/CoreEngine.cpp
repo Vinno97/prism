0af3b0551d0e5cc82a98f39e8c66d60f954c7ede
Daan Willems <dwillems2@avans.nl>
2018-10-07 23:15:05 +0200

    Add some test code WIP with changing to radians

--- Prism.Engine/src/CoreEngine.cpp
+++ Prism.Engine/src/CoreEngine.cpp
@@ -1,8 +1,16 @@
 #include "CoreEngine.h"
+#include "Renderer/Graphics/Models/Model.h"
+#include "Renderer/Renderable.h"
+#include "Renderer/Graphics/Loader/ModelLoader.h"
+#include "Renderer/ForwardRenderer.h"
+#include "Renderer/Camera.h"
 #include <tuple>
 #include <vector>
 
 using namespace std;
+using namespace Renderer::Graphics::Loader;
+using namespace Renderer::Graphics::Models;
+using namespace Renderer;
 
 const void CoreEngine::CreateWindow(const char* title, const int width, const int height, const int x, const int y) {
 	context.window->init(title, width, height, x, y);
@@ -16,6 +24,35 @@ void CoreEngine::Run()
 	auto lastTime = std::chrono::system_clock::now();
 	int count = 0;
 
+	ModelLoader ml = ModelLoader();
+	Model* m = ml.loadModel("./res/bunny.obj");
+
+	vector<Renderable> renderables;
+
+	Renderable renderable;
+	renderable.model = m;
+	get<0>(renderable.position) = 0.f;
+	get<1>(renderable.position) = -1.f;
+	get<2>(renderable.position) = -6.f;
+	get<0>(renderable.scale) = 10;
+	get<1>(renderable.scale) = 10;
+	get<2>(renderable.scale) = 10;
+	get<0>(renderable.rotation) = 3.14f;
+
+	renderables.push_back(renderable);
+	get<0>(renderable.position) = -2.f;
+	get<1>(renderable.position) = -1.f;
+	get<2>(renderable.position) = -3.f;
+	renderables.push_back(renderable);
+	get<0>(renderable.position) = 2.f;
+	get<1>(renderable.position) = -1.f;
+	get<2>(renderable.position) = -3.f;
+	renderables.push_back(renderable);
+
+	ForwardRenderer* fw = new ForwardRenderer(1920, 1080);
+	Camera c = Camera();
+	c.move(0.0f, 0.0f, 3.f);
+
 	//While the window is unclosed run the gameloop
 	while (!context.window->shouldClose()) 
 	{
@@ -24,10 +61,12 @@ void CoreEngine::Run()
 		//Deltatime in microseconds
 		auto deltaTime = std::chrono::duration_cast<std::chrono::microseconds>(startTime - lastTime); 
 		lastTime = startTime;
-
+		//c.rotate(0.0f, 3.14f, 0.f);
 		//Sets the right values in context
 		context.deltaTime = deltaTime.count();
 		context.stateMachine->getCurrentState()->onUpdate(context);
+		fw->draw(&c, renderables);
+		context.window->swapScreen();
 	}
 }
 
