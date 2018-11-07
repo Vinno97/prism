f828401854547212d3b8601764055abf14813099
cornest <corne.steenbakkers@gmail.com>
2018-11-07 19:50:32 +0100

    get all health entities

--- Prism.Engine/src/ECS/Systems/AttackSystem.cpp
+++ Prism.Engine/src/ECS/Systems/AttackSystem.cpp
@@ -1,9 +1,7 @@
 #include <math.h>
 #include "ECS/Systems/AttackSystem.h"
-#include "ECS/Components/DragComponent.h"
-#include "ECS/Components/PositionComponent.h"
-#include "ECS/Components/VelocityComponent.h"
 #include "ECS/Components/HealthComponent.h"
+
 
 namespace ECS {
 	namespace Systems {
@@ -14,6 +12,14 @@ namespace ECS {
 		AttackSystem::~AttackSystem() = default;
 
 		void AttackSystem::update(Context context) {
+			auto healthEntities = this->entityManager->getAllEntitiesWithComponent<HealthComponent>();
+
+			//while (appearanceEntities[0].component->health > 0) {
+			//	cout << appearanceEntities[0].component->health << endl;
+
+			//	appearanceEntities[0].component->health = appearanceEntities[0].component->health - 10;
+			//}				
+
 
 		}
 	}
