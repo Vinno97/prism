b3425f5c130648b1783747fc81be1fa2199cf2f9
Daan Willems <dwillems2@avans.nl>
2018-11-13 12:30:56 +0100

    Update clone function

--- Prism.Game/include/ECS/Components/EnemySpawnComponent.h
+++ Prism.Game/include/ECS/Components/EnemySpawnComponent.h
@@ -6,14 +6,14 @@ namespace ECS {
 	namespace Components {
 		struct EnemySpawnComponent : Component {
 			EnemySpawnComponent() = default;
-			EnemySpawnComponent(float interval) : spawnInterval{ interval } {};
+			EnemySpawnComponent(float interval, float timeSinceLastSpawn, bool enabled) : spawnInterval{ interval }, timeSinceLastSpawn{ timeSinceLastSpawn }, enabled{ enabled } {};
 
 			float spawnInterval = 10; // Spawn a new enemy when this time has passed (in seconds)
 			float timeSinceLastSpawn = 0;
 			bool enabled = true;
 
 			Component* Clone() override {
-				return new EnemySpawnComponent{ spawnInterval };
+				return new EnemySpawnComponent{ spawnInterval, timeSinceLastSpawn, enabled };
 			}
 		};
 	}
