c5fd5566fa1bf03570d646b0b9130310c56ffb68
cornest_ <corne.steenbakkers@gmail.com>
2018-11-15 16:17:56 +0100

    Cheat: Increase health (key H)

--- Prism.Game/src/ECS/Systems/KeyboardInputSystem.cpp
+++ Prism.Game/src/ECS/Systems/KeyboardInputSystem.cpp
@@ -1,16 +1,24 @@
 #pragma once
-
+#include <thread>
 #include "Context.h"
 #include "InputManager.h"
 
 #include "ECS/Systems/KeyboardInputSystem.h"
 #include "ECS/Components/VelocityComponent.h"
 #include "ECS/Components/AppearanceComponent.h"
+#include "ECS/Components/PlayerComponent.h"
+#include "ECS/Components/HealthComponent.h"
 #include "ECS/Components/KeyboardInputComponent.h"
 
 namespace ECS {
 	namespace Systems {
 		KeyboardInputSystem::KeyboardInputSystem(EntityManager &entityManager) : System(entityManager) {
+		}
+
+
+		void increaseHealth() {
+	
+	;
 		}
 
 		KeyboardInputSystem::~KeyboardInputSystem()
@@ -53,8 +61,21 @@ namespace ECS {
 						appearance->rotationY += context.deltaTime * 50;
 					}
 				}
+
+				// Cheat option to increase health of the Player
+				if (entityManager->hasComponent<PlayerComponent>(entity.id)) {
+
+					auto healthComponent = entityManager->getComponent<HealthComponent>(entity.id);
+
+					if (input->isKeyPressed(Key::KEY_H))
+					{
+								healthComponent->health += 100;
+								cout << healthComponent->health;
+					}
+				}
 			}
 		}
+
 		System * KeyboardInputSystem::clone()
 		{
 			auto test = new KeyboardInputSystem(*entityManager);
--- Prism.Game/src/States/PrismGame.cpp
+++ Prism.Game/src/States/PrismGame.cpp
@@ -94,7 +94,7 @@ namespace States {
 		for (auto &entity : entityManager.getAllEntitiesWithComponent<VelocityComponent>()) {
 			auto velocity = entity.component;
 			auto position = entityManager.getComponent<PositionComponent>(entity.id);
-			printf("Entity:\t\t%d \nPosition: \tX: %.2f, Y: %.2f\nVelocity:\tdX: %.2f, dY: %.2f\n\n", entity.id, position->x, position->y, velocity->dx, velocity->dy);
+			//printf("Entity:\t\t%d \nPosition: \tX: %.2f, Y: %.2f\nVelocity:\tdX: %.2f, dY: %.2f\n\n", entity.id, position->x, position->y, velocity->dx, velocity->dy);
 		}
 
 		context.window->swapScreen();
