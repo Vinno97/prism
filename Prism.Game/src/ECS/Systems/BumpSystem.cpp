e03f9cfd25e4e0bc2e4c5ffe69b977f778aa499a
Louis Lim <llylim@avans.nl>
2018-11-19 00:28:55 +0100

    fixed bug in corners of fort

--- Prism.Game/src/ECS/Systems/BumpSystem.cpp
+++ Prism.Game/src/ECS/Systems/BumpSystem.cpp
@@ -23,7 +23,7 @@ void BumpSystem::update(Context context)
 			const auto boundingBox = &boundingBoxComponent->boundingBox;
 			auto vector = &boundingBoxComponent->collidesWith;
 			
-			if (AmountCollisions(*boundingBox, *boundingBox, *vector) > 0) {
+			if (CountCollisions(*boundingBox, *boundingBox, *vector) > 0) {
 				const auto position = entityManager->getComponent<PositionComponent>(entity.id);
 				auto velocity = entityManager->getComponent<VelocityComponent>(entity.id);
 
@@ -32,32 +32,31 @@ void BumpSystem::update(Context context)
 				float x = position->x;
 				float y = position->y;
 
-				testBox1.SetPosXY(x -= velocity->dx*context.deltaTime, y);
-				testBox2.SetPosXY(x, y -= velocity->dy*context.deltaTime);
+				testBox1.SetPosXY(x - velocity->dx*context.deltaTime, y);
+				testBox2.SetPosXY(x, y - velocity->dy*context.deltaTime);
 
-				if (AmountCollisions(testBox1, *boundingBox, *vector) == 0) {
+				if (CountCollisions(testBox1, *boundingBox, *vector) == 0) {
 					position->x -= velocity->dx*context.deltaTime;
 					velocity->dx = 0;
 				}
-				else if (AmountCollisions(testBox2, *boundingBox, *vector) == 0) {
+				else if (CountCollisions(testBox2, *boundingBox, *vector) == 0) {
 					position->y -= velocity->dy*context.deltaTime;
 					velocity->dy = 0;
 				}
 				else {
-					position->x -= velocity->dx*context.deltaTime;
+					position->x -= velocity->dx * context.deltaTime;
 					velocity->dx = 0;
-					position->y -= velocity->dy*context.deltaTime;
+					position->y -= velocity->dy * context.deltaTime;
 					velocity->dy = 0;
-
 				}
+				boundingBoxComponent->didCollide = false;
+				boundingBoxComponent->collidesWith.clear();
 			}
-			boundingBoxComponent->didCollide = false;
-			boundingBoxComponent->collidesWith.clear();
 		}
 	}
 }
 
-int BumpSystem::AmountCollisions(BoundingBox &box1, BoundingBox &adress, std::vector<const BoundingBox*> &vector)
+int BumpSystem::CountCollisions(BoundingBox &box1, BoundingBox &adress, std::vector<const BoundingBox*> &vector)
 {
 	int count = 0;
 	for (int i = 0; i < vector.size();i++) {
