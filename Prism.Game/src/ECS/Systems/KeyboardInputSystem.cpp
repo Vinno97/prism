95a79e51ecb03aa5152d66761c6af14f775ffee9
Ben Krocké <benkrocke@hotmail.com>
2018-11-26 14:48:28 +0100

    Added 3 levels

--- Prism.Game/src/ECS/Systems/KeyboardInputSystem.cpp
+++ Prism.Game/src/ECS/Systems/KeyboardInputSystem.cpp
@@ -25,7 +25,7 @@ namespace ECS {
 		void KeyboardInputSystem::update(Context& context) {
 			// 1 unit/second^2
 			//double acceleration = 5;
-			double acceleration = 5;
+			double acceleration = 10;
 
 			auto input = context.inputManager;
 			
