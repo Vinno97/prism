cc96e1558437c21f7f6563144c242f13176c50ac
photonman1997 <jmvugt1@avans.nl>
2018-11-28 13:44:46 +0100

    small screen (again...)

--- Prism.Engine/src/Renderer/Graphics/OpenGL/OGLTexture.cpp
+++ Prism.Engine/src/Renderer/Graphics/OpenGL/OGLTexture.cpp
@@ -28,7 +28,7 @@ namespace Renderer {
 				glBindTexture(GL_TEXTURE_2D, textureID);
 
 				// Give an empty image to OpenGL ( the last "0" )
-				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1920, 1080, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+				glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 1920 / 2, 1080 / 2, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
 
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
