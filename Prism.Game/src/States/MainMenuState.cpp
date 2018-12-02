17776ac4ffa6da18cef9953cdad326fab446359a
Daan Willems <dwillems2@avans.nl>
2018-12-02 17:02:54 +0100

    Re enable callback

--- Prism.Game/src/States/MainMenuState.cpp
+++ Prism.Game/src/States/MainMenuState.cpp
@@ -16,8 +16,8 @@ namespace States {
 		PrismGame game = PrismGame();
 		context.stateMachine->addState<PrismGame>(context);
 
-		//std::function<void()> callback = [context]() { context.stateMachine->setState<PrismGame>(); };
-		menuBuilder.addControl(-0.35,  0.4, 0.6, 0.18, "img/NewGameButton.png");
+		std::function<void()> callback = [context]() { context.stateMachine->setState<PrismGame>(); };
+		menuBuilder.addControl(-0.35,  0.4, 0.6, 0.18, "img/NewGameButton.png", callback);
 		menuBuilder.addControl(-0.35,  0.1, 0.6, 0.18, "img/LoadGameButton.png");
 		menuBuilder.addControl(-0.35, -0.7, 0.6, 0.18, "img/QuitGameButton.png");
 
