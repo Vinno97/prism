726e1a0037d10abbb7ac7f56144fd3cedaedf249
Daan Willems <dwillems2@avans.nl>
2018-12-15 13:59:28 +0100

    Add comment

--- Prism.Engine/include/ECS/Components/AnimationComponent.h
+++ Prism.Engine/include/ECS/Components/AnimationComponent.h
@@ -19,6 +19,7 @@ namespace ECS
 			//Max is always 100
 			//To start animation add new animation to map and set float value anywhere from 0 to 100
 			//Animation will automatically be removed when finished
+			//The boolean indicates whether the entity should be removed when the animation is finished
 			std::map<Renderer::Animation, std::tuple<float, bool>> currentAnimations;
 
 			std::unique_ptr<Component> clone() override
